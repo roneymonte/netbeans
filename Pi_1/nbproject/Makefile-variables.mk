977bcde3d24bac15294bbcfd307d97a0cd6fbee8
roneymonte <roneymonte@gmail.com>
2014-10-06 21:25:56 -0300

    calculo de PI

---
+++ Pi_1/nbproject/Makefile-variables.mk
@@ -0,0 +1,59 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Serial configuration
+CND_PLATFORM_Serial=GNU-MacOSX
+CND_ARTIFACT_DIR_Serial=dist/Serial/GNU-MacOSX
+CND_ARTIFACT_NAME_Serial=pi_1
+CND_ARTIFACT_PATH_Serial=dist/Serial/GNU-MacOSX/pi_1
+CND_PACKAGE_DIR_Serial=dist/Serial/GNU-MacOSX/package
+CND_PACKAGE_NAME_Serial=pi1.tar
+CND_PACKAGE_PATH_Serial=dist/Serial/GNU-MacOSX/package/pi1.tar
+# Pthreads configuration
+CND_PLATFORM_Pthreads=GNU-MacOSX
+CND_ARTIFACT_DIR_Pthreads=dist/Pthreads/GNU-MacOSX
+CND_ARTIFACT_NAME_Pthreads=pi_1
+CND_ARTIFACT_PATH_Pthreads=dist/Pthreads/GNU-MacOSX/pi_1
+CND_PACKAGE_DIR_Pthreads=dist/Pthreads/GNU-MacOSX/package
+CND_PACKAGE_NAME_Pthreads=pi1.tar
+CND_PACKAGE_PATH_Pthreads=dist/Pthreads/GNU-MacOSX/package/pi1.tar
+# Pthreads_safe configuration
+CND_PLATFORM_Pthreads_safe=GNU-MacOSX
+CND_ARTIFACT_DIR_Pthreads_safe=dist/Pthreads_safe/GNU-MacOSX
+CND_ARTIFACT_NAME_Pthreads_safe=pi_1
+CND_ARTIFACT_PATH_Pthreads_safe=dist/Pthreads_safe/GNU-MacOSX/pi_1
+CND_PACKAGE_DIR_Pthreads_safe=dist/Pthreads_safe/GNU-MacOSX/package
+CND_PACKAGE_NAME_Pthreads_safe=pi1.tar
+CND_PACKAGE_PATH_Pthreads_safe=dist/Pthreads_safe/GNU-MacOSX/package/pi1.tar
+# Pthread_Hot configuration
+CND_PLATFORM_Pthread_Hot=GNU-MacOSX
+CND_ARTIFACT_DIR_Pthread_Hot=dist/Pthread_Hot/GNU-MacOSX
+CND_ARTIFACT_NAME_Pthread_Hot=pi_1
+CND_ARTIFACT_PATH_Pthread_Hot=dist/Pthread_Hot/GNU-MacOSX/pi_1
+CND_PACKAGE_DIR_Pthread_Hot=dist/Pthread_Hot/GNU-MacOSX/package
+CND_PACKAGE_NAME_Pthread_Hot=pi1.tar
+CND_PACKAGE_PATH_Pthread_Hot=dist/Pthread_Hot/GNU-MacOSX/package/pi1.tar
+# OpenMP configuration
+CND_PLATFORM_OpenMP=GNU-MacOSX
+CND_ARTIFACT_DIR_OpenMP=dist/OpenMP/GNU-MacOSX
+CND_ARTIFACT_NAME_OpenMP=pi_1
+CND_ARTIFACT_PATH_OpenMP=dist/OpenMP/GNU-MacOSX/pi_1
+CND_PACKAGE_DIR_OpenMP=dist/OpenMP/GNU-MacOSX/package
+CND_PACKAGE_NAME_OpenMP=pi1.tar
+CND_PACKAGE_PATH_OpenMP=dist/OpenMP/GNU-MacOSX/package/pi1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Pi_1/nbproject/private/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Serial configuration
+# Pthreads configuration
+# Pthreads_safe configuration
+# Pthread_Hot configuration
+# OpenMP configuration
